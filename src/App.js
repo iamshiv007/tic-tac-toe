5f6edf19a6ce9c0dfdc3492f9873b641f518885a
iamshiv007 <iamshiv20032003@gmail.com>
2023-06-11 00:11:25 +0530

    style completed and playGame function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,56 @@
 import "./App.css";
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [value, setValue] = useState(0);
+
+  const playGame = (e) => {
+    if (!e.target.innerText) {
+      e.target.innerText = value;
+      setValue(value === 0 ? "❌ " : 0);
+    }
+  };
+
   return (
     <>
-      <div>Hello</div>
+      <div className="header">Tic-Tac-toe</div>
+      <div className="mainContainer">
+        <FirstColumn playGame={playGame} />
+        <SecondColumn playGame={playGame} />
+        <ThirdColumn playGame={playGame} />
+      </div>
     </>
   );
-}
+};
 
 export default App;
+
+const FirstColumn = ({ playGame }) => {
+  return (
+    <div>
+      <div onClick={playGame} className="one four seven"></div>
+      <div onClick={playGame} className="one five"></div>
+      <div onClick={playGame} className="one six eight"></div>
+    </div>
+  );
+};
+
+const SecondColumn = ({ playGame }) => {
+  return (
+    <div>
+      <div onClick={playGame} className="two four"></div>
+      <div onClick={playGame} className="two five seven eight"></div>
+      <div onClick={playGame} className="two six"></div>
+    </div>
+  );
+};
+
+const ThirdColumn = ({ playGame }) => {
+  return (
+    <div>
+      <div onClick={playGame} className="three four eight"></div>
+      <div onClick={playGame} className="three five"></div>
+      <div onClick={playGame} className="three six seven"></div>
+    </div>
+  );
+};
